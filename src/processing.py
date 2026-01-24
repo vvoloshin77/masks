b63c56f1e8b8d87a5b2ddf375ff3ad2a921ebefb
kimikosi_77 <usgeru@yandex.ru>
2026-01-24 20:04:47 +0300

    homework_10_1 is done

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
-def filter_by_state(data: list[dict[str, any]], state="EXECUTED") -> list[dict[str, any]]:
+from typing import Any
+
+def filter_by_state(data: list[dict[str, Any]], state="EXECUTED") -> list[dict[str, Any]]:
     """Выводит словари, которые содержат указанный ключ"""
 
     result = []
@@ -10,7 +12,7 @@ def filter_by_state(data: list[dict[str, any]], state="EXECUTED") -> list[dict[s
     return result
 
 
-def sort_by_date(data: list[dict[str, any]], reverse=True) -> list[dict[str, any]]:
+def sort_by_date(data: list[dict[str, Any]], reverse=True) -> list[dict[str, Any]]:
     """Функция возвращает отсортированный по дате список"""
 
     return sorted(data, key=lambda x: x["date"], reverse=reverse)
