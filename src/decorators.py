9ae48ed3780b0105b89a059f0f9fe5077f9eba34
kimikosi_77 <usgeru@yandex.ru>
2026-02-11 00:25:02 +0300

    add docstr

--- src/decorators.py
+++ src/decorators.py
@@ -3,8 +3,11 @@ from typing import Any, Callable, Optional
 
 
 def log(filename: Optional[str] = None) -> Callable:
+    """Функция на вход принимает файл"""
     def decorator(func: Callable) -> Callable:
+        """Функция на вход принимает функцию"""
         def wrapper(*args: Any, **kwargs: Any) -> Any:
+            """Функция на вход принимает параметры"""
             try:
                 result = func(*args, **kwargs)
                 log_message = f"{func.__name__} ok\n"
