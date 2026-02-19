4491416777bb167fb4de0bc6671ecd1eb3144876
kimikosi_77 <usgeru@yandex.ru>
2026-02-19 18:39:57 +0300

    fixed func

--- src/external_api.py
+++ src/external_api.py
@@ -3,7 +3,6 @@ import requests
 from pprint import pprint
 from utils import transaction_amount
 from dotenv import load_dotenv
-import time
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -12,37 +11,21 @@ API_KEY = os.getenv("API_KEY")
 def get_currency_rate(transaction_list: list[dict]) -> float:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции"""
 
-    transactions_in_rub = []
-    for transaction in transaction_list[:2]:
-        operation_amount = transaction.get("operationAmount")
+    data = transaction_list[1]
 
-        if not operation_amount:
-            continue
-        amount = float(operation_amount["amount"])
-        currency_code = operation_amount["currency"]["code"]
+    currency_code = data['operationAmount']['currency']['code']
+    op_amount = float(data['operationAmount']['amount'])
 
-        if currency_code == "RUB":
-            transactions_in_rub.append(amount)
-        else:
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency_code}&amount={amount}"
-            headers = {"apikey": f"{API_KEY}"}
+    if currency_code != "RUB":
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency_code}&amount={op_amount}"
+        headers = {"apikey": f"{API_KEY}"}
 
-            response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-            if response.status_code == 200:
-                result = response.json()
-                transactions_in_rub.append(result["result"])
-
-            elif response.status_code == 429:
-                print("Rate limit exceeded. Waiting...")
-                time.sleep(10)
-                continue
-
-            else:
-                raise ValueError(f"Error fetching currency rate: {response.status_code}")
-
-
-    return transactions_in_rub
+        result = response.json()
+        return result
+    else:
+        return op_amount
 
 
 if __name__ == "__main__":  # pragma: no cover
