9f94644311acf641164aced5db87011fc795fee9
kimikosi_77 <usgeru@yandex.ru>
2026-02-25 19:12:58 +0300

    fixed func external_api

--- src/external_api.py
+++ src/external_api.py
@@ -13,7 +13,6 @@ API_KEY = os.getenv("API_KEY")
 def get_currency_rate(transaction: dict) -> float:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции"""
 
-
     currency_code = transaction['operationAmount']['currency']['code']
     op_amount = float(transaction['operationAmount']['amount'])
 
@@ -21,7 +20,7 @@ def get_currency_rate(transaction: dict) -> float:
         url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency_code}&amount={op_amount}"
         headers = {"apikey": f"{API_KEY}"}
 
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, timeout=10)
 
         result = response.json()
         return result
@@ -30,6 +29,37 @@ def get_currency_rate(transaction: dict) -> float:
 
 
 if __name__ == "__main__":  # pragma: no cover
-    transactions = transaction_amount("../data/operations.json")
-    rate = get_currency_rate(transactions[0])
-    pprint(rate)
+    x_rub = {
+        "id": 121646999,
+        "state": "CANCELED",
+        "date": "2018-06-08T16:14:59.936274",
+        "operationAmount": {
+            "amount": "91121.62",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 7552745726849311",
+        "to": "Счет 34799481846914116850"
+    }
+
+    x_usd = {
+        "id": 179194306,
+        "state": "EXECUTED",
+        "date": "2019-05-19T12:51:49.023880",
+        "operationAmount": {
+            "amount": "6381.58",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "МИР 5211277418228469",
+        "to": "Счет 58518872592028002662"
+    }
+
+    pprint(get_currency_rate(x_rub))
+    pprint(get_currency_rate(x_usd))
