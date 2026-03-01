0b1234c6f511fbd276943405478ce5c36ea85eda
kimikosi_77 <usgeru@yandex.ru>
2026-03-01 22:01:51 +0300

    fixed func masks with logs

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,17 @@
 import logging
+#import os
 
-logger = logging.getLogger('masks')
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
-    filename='logs/masks.log',
-    filemode='w'
-)
+#if not os.path.exists('logs'):
+    #os.makedirs('logs')
+
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('../logs/masks.log', mode='w', encoding='UTF-8')
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
 
 def get_mask_card_number(card_number: int) -> str:
     """Функция на вход принмает номер карты в виде число и возвращает маску"""
@@ -15,13 +20,11 @@ def get_mask_card_number(card_number: int) -> str:
         logger.error("Номер карты введен неверно")
         return "Номер карты введен неверно"
     if len(card_number_str) == 16:
+        logger.info("Функция работает корректно, номер маскируется")
         return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
     else:
+        logger.info("Функция работает корректно, номер маскируется")
         return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
-
-
-# if __name__ == "__main__":
-# print(get_mask_card_number(8994567891011123099905558))
 
 
 # def get_mask_card_number(card_number: int) -> str:
@@ -42,10 +45,13 @@ def get_mask_account(account_number: int) -> str:
     """Функция на вход принмает номер счета в виде число и возвращает маску"""
     account_number_str = str(account_number).replace(" ", "").replace("-", "")
     if len(account_number_str) < 10:
+        logger.error("Номер карты введен неверно")
         return "Номер счета введен неверно"
     else:
+        logger.info("Функция работает корректно, номер маскируется")
         return f"**{account_number_str[-4:]}"
 
 
-if __name__ == "__main__": # pragma: no cover
- print(get_mask_account(89945678910111213141516))
+if __name__ == "__main__":  # pragma: no cover
+    print(get_mask_account(89945678910111213141516))
+    print(get_mask_card_number(8994567891011123099905558))
